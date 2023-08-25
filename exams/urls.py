61603d9c4ce9b57d83569ea1e99d2dcdec508a93
jdongdong96 <zkfkd1030@naver.com>
2023-08-25 12:17:15 +0900

    기존 exam에서 테스트점수 이력 구현 및 ExamsShow에 난이도를 저장하기 위한 수정(line92)

--- exams/urls.py
+++ exams/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from exams import views
 
 urlpatterns = [
-    path("Word_Test_History/", views.Word_Test_History, name="Word_Test_History"),
-    path("Word_Test_Score/", views.Word_Test_Score, name="Word_Test_Score"),
+    path("Word_Test_History/", views.Word_Test_History.as_view(), name="Word_Test_History"),
+    path("Word_Test_Score/", views.WordTestScore.as_view(), name="Word_Test_Score"),
 
     # 용석 작업 - 기존 작업 유지 하고 추가 작업 합니다. - 나중에 확인 하세요.
     path("exam-setting/", views.ExamsSetting.as_view(), name="exam-setting"),
